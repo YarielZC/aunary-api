97c1614df8f3a0ecdaffd37ec6a4f10b545bd792
YarielZC <yarielzamoracueto@gmail.com>
2026-06-12 16:44:17 -0400

    chore(main): register auth and dev routers

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .modules.users.router import router as users_router
+from .modules.auth.router import router as auth_router
+from .modules.dev.router import router as dev_router
 
 app = FastAPI(title="Aunary API")
 
 app.include_router(users_router)
+app.include_router(auth_router)
+app.include_router(dev_router)
 
 app.add_middleware(
     CORSMiddleware,
